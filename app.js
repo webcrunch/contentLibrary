0f85154273dd0cb5a3e1e92fbc14d826f5846708
webcrunch <jarllindquist@gmail.com>
2016-08-21 13:25:00 +0200

    restart

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var multer = require('multer');
+var app = express();
+var port = 5000;
+
+app.set('port', port); 
+
+var storage = multer.diskStorage({
+  destination: function (request, file, callback) {
+    callback(null, '/example/uploads');
+  },
+  filename: function (request, file, callback) {
+    console.log(file);
+    callback(null, file.originalname)
+  }
+});
+
+
+var upload = multer({storage: storage}).single('photo');
+
+//Showing index.html file on our homepage
+
+
+//Posting the file upload
+app.post('/upload', function(request, response) {
+  upload(request, response, function(err) {
+  if(err) {
+    console.log('Error Occured');
+    return;
+  }
+  console.log(request.file);
+  response.end('Your File Uploaded');
+  console.log('Photo Uploaded');
+  })
+});
+
+
+app.use(express.static('www'));
+
+var server = app.listen(port, function () {
+  console.log('Listening on port ' + server.address().port)
+});
